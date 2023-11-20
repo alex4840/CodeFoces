1b19238a901efa5c6538f494b433850a03b5f8b2
alex4840 <119257376+alex4840@users.noreply.github.com>
2023-11-20 22:51:40 +0530

    Create 281A.cpp

---
+++ 281A.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    string s;
+    cin >> s;
+
+    if (s.size() > 0) {
+        if (s[0] >= 'a' && s[0] <= 'z') {
+            s[0] = s[0] - 32;
+            cout << s << endl;
+        } else {
+            cout << s << endl;
+        }
+    } else {
+        return 0;
+    }
+}
